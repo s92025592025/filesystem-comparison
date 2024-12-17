45967465ae836f67f88fdffec6ebe2908f8708f0
Daniel Wang <s92025592025@gmail.com>
2024-12-16 22:48:18 -0800

    change for packahing

--- src/main/java/com/flyingapplepie/tool/model/ComparedRow.java
+++ src/main/java/com/flyingapplepie/tool/model/ComparedRow.java
@@ -1,11 +1,19 @@
 package com.flyingapplepie.tool.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ComparedRow {
+    @JsonProperty("filePath")
     private String filePath;
+
+    @JsonProperty("mainSystemChecksum")
     private String mainSystemChecksum;
+
+    @JsonProperty("comparisonSystemChecksum")
     private String comparisonSystemChecksum;
+
+    @JsonProperty("isSame")
     private boolean isSame;
 }
