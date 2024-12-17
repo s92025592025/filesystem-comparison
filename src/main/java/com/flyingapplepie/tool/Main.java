8782800722bd873c8d641513fb39b4b367d419f9
Daniel Wang <s92025592025@gmail.com>
2024-12-16 21:02:00 -0800

    fix-tyoe

--- src/main/java/com/flyingapplepie/tool/Main.java
+++ src/main/java/com/flyingapplepie/tool/Main.java
@@ -1,7 +1,23 @@
 package com.flyingapplepie.tool;
 
+import org.apache.commons.cli.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Options cmdOptions = new Options();
+        cmdOptions.addOption("h", "help", false, "Help command");
+        cmdOptions.addOption("mf", "main-fs", true, "Main file system when comparing");
+        cmdOptions.addOption("rf", "reference-fs", true, "Reference file system when comparing");
+
+        CommandLineParser cmdParser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = cmdParser.parse(cmdOptions, args);
+        } catch (ParseException e) {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("filesystem-comparison", cmdOptions);
+        }
     }
 }
