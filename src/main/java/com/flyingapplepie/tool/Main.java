65415c6f710b1e7ed54a86a1984e4bc52247a25b
Daniel Wang <s92025592025@gmail.com>
2024-12-17 16:24:21 -0800

    when parameter is missing, show error and help text

--- src/main/java/com/flyingapplepie/tool/Main.java
+++ src/main/java/com/flyingapplepie/tool/Main.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.SequenceWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.flyingapplepie.tool.model.ComparedRow;
+import com.flyingapplepie.tool.util.CommandlineHandler;
 import com.flyingapplepie.tool.utlity.FileSha256Calculator;
 import org.apache.commons.cli.*;
 
@@ -21,63 +22,61 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Options cmdOptions = new Options();
-        cmdOptions.addOption("h", "help", false, "Help command");
-        cmdOptions.addOption("mf", "main-fs", true, "Main file system base directory when comparing");
-        cmdOptions.addOption("rf", "reference-fs", true, "Reference file system base directory when comparing");
-        cmdOptions.addOption("o", "output", true, "Where the output csv file should go");
-
-        CommandLineParser cmdParser = new DefaultParser();
-        CommandLine cmd = null;
+        CommandlineHandler commandlineHandler = new CommandlineHandler();
 
         try {
-            cmd = cmdParser.parse(cmdOptions, args);
-            Path mainFileSystemBase = new File(cmd.getOptionValue("mf")).toPath();
-            Path referenceFileSystemBase = new File(cmd.getOptionValue("rf")).toPath();
-            Path outputCsvFilePath = new File(cmd.getOptionValue("o")).toPath();
+            CommandLine cmd = commandlineHandler.parseCmdInput(args);
 
-            CsvMapper csvMapper = new CsvMapper();
-            CsvSchema csvSchema = CsvSchema.builder()
-                    .setUseHeader(true)
-                    .addColumn("filePath")
-                    .addColumn("mainSystemChecksum")
-                    .addColumn("comparisonSystemChecksum")
-                    .addColumn("isSame")
-                    .build();
+            if (cmd.hasOption("mf") && cmd.hasOption("rf") && cmd.hasOption("o")) {
+                Path mainFileSystemBase = new File(cmd.getOptionValue("mf")).toPath();
+                Path referenceFileSystemBase = new File(cmd.getOptionValue("rf")).toPath();
+                Path outputCsvFilePath = new File(cmd.getOptionValue("o")).toPath();
 
-            try (SequenceWriter sequenceWriter = csvMapper.writerFor(ComparedRow.class).with(csvSchema).writeValues(outputCsvFilePath.toFile())) {
-                try (Stream<Path> fsWalker = Files.walk(mainFileSystemBase)) {
-                    fsWalker.filter(Files::isRegularFile)
-                            .forEach(mainFsFilePath -> {
-                                Path relativePath = mainFileSystemBase.relativize(mainFsFilePath);
-                                Path referenceFsFilePath = referenceFileSystemBase.resolve(relativePath);
+                CsvMapper csvMapper = new CsvMapper();
+                CsvSchema csvSchema = CsvSchema.builder()
+                        .setUseHeader(true)
+                        .addColumn("filePath")
+                        .addColumn("mainSystemChecksum")
+                        .addColumn("comparisonSystemChecksum")
+                        .addColumn("isSame")
+                        .build();
 
-                                Map<Path, FileSha256Calculator> processingBucket = new HashMap<>();
-                                processingBucket.put(mainFsFilePath, new FileSha256Calculator(mainFsFilePath));
-                                processingBucket.put(referenceFsFilePath, new FileSha256Calculator(referenceFsFilePath));
+                try (SequenceWriter sequenceWriter = csvMapper.writerFor(ComparedRow.class).with(csvSchema).writeValues(outputCsvFilePath.toFile())) {
+                    try (Stream<Path> fsWalker = Files.walk(mainFileSystemBase)) {
+                        fsWalker.filter(Files::isRegularFile)
+                                .forEach(mainFsFilePath -> {
+                                    Path relativePath = mainFileSystemBase.relativize(mainFsFilePath);
+                                    Path referenceFsFilePath = referenceFileSystemBase.resolve(relativePath);
 
-                                Map<Path, String> processedChecksums = processingBucket.entrySet().parallelStream()
-                                        .collect(Collectors.toMap(
-                                                Map.Entry::getKey,
-                                                entry -> entry.getValue().getChecksumString()
-                                        ));
+                                    Map<Path, FileSha256Calculator> processingBucket = new HashMap<>();
+                                    processingBucket.put(mainFsFilePath, new FileSha256Calculator(mainFsFilePath));
+                                    processingBucket.put(referenceFsFilePath, new FileSha256Calculator(referenceFsFilePath));
 
-                                ComparedRow comparedRow = new ComparedRow();
-                                comparedRow.setFilePath(mainFsFilePath.toAbsolutePath().toString());
-                                comparedRow.setMainSystemChecksum(processedChecksums.get(mainFsFilePath));
-                                comparedRow.setComparisonSystemChecksum(processedChecksums.get(referenceFsFilePath));
-                                comparedRow.setSame(comparedRow.getMainSystemChecksum().equals(comparedRow.getComparisonSystemChecksum()));
-                                try {
-                                    sequenceWriter.write(comparedRow);
-                                } catch (IOException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            });
+                                    Map<Path, String> processedChecksums = processingBucket.entrySet().parallelStream()
+                                            .collect(Collectors.toMap(
+                                                    Map.Entry::getKey,
+                                                    entry -> entry.getValue().getChecksumString()
+                                            ));
+
+                                    ComparedRow comparedRow = new ComparedRow();
+                                    comparedRow.setFilePath(mainFsFilePath.toAbsolutePath().toString());
+                                    comparedRow.setMainSystemChecksum(processedChecksums.get(mainFsFilePath));
+                                    comparedRow.setComparisonSystemChecksum(processedChecksums.get(referenceFsFilePath));
+                                    comparedRow.setSame(comparedRow.getMainSystemChecksum().equals(comparedRow.getComparisonSystemChecksum()));
+                                    try {
+                                        sequenceWriter.write(comparedRow);
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                });
+                    }
                 }
+            } else {
+                System.err.println("Flags -md, -rf, and -o are required, please see the following usage for more details...");
+                commandlineHandler.printHelp();
             }
         } catch (ParseException e) {
-            HelpFormatter helpFormatter = new HelpFormatter();
-            helpFormatter.printHelp("filesystem-comparison: Check if the files on main file system also exists on the reference file system, and compare the checksum", cmdOptions);
+            commandlineHandler.printHelp();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
---
+++ src/main/java/com/flyingapplepie/tool/util/CommandlineHandler.java
@@ -0,0 +1,37 @@
+package com.flyingapplepie.tool.util;
+
+import org.apache.commons.cli.*;
+
+public class CommandlineHandler {
+    private final Options cmdOptions;
+
+    public CommandlineHandler() {
+        this.cmdOptions = new Options();
+        cmdOptions.addOption("h", "help", false, "Help command");
+        cmdOptions.addOption("mf", "main-fs", true, "Main file system base directory when comparing");
+        cmdOptions.addOption("rf", "reference-fs", true, "Reference file system base directory when comparing");
+        cmdOptions.addOption("o", "output", true, "Where the output csv file should go");
+    }
+
+    /**
+     * @param args Arguments array passed into main function
+     * @return {@link CommandLine} with parsed commandline input
+     * @throws ParseException When unknown flags are passed
+     */
+    public CommandLine parseCmdInput(String[] args) throws ParseException {
+        CommandLineParser cmdParser = new DefaultParser();
+
+        return cmdParser.parse(this.cmdOptions, args);
+    }
+
+    /**
+     * Prints cli help message
+     */
+    public void printHelp() {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        helpFormatter.printHelp(
+                "filesystem-comparison: Check if the files on main file system also exists on the reference file system, and compare the checksum",
+                this.cmdOptions
+        );
+    }
+}
