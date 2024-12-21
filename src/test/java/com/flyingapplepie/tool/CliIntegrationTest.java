8ba8b74b9d49e28b9896a0485b7ee1ef06699d89
Daniel Wang <s92025592025@gmail.com>
2024-12-21 09:41:57 -0800

    can test error outout

--- src/test/java/com/flyingapplepie/tool/CliIntegrationTest.java
+++ src/test/java/com/flyingapplepie/tool/CliIntegrationTest.java
@@ -1,10 +1,41 @@
 package com.flyingapplepie.tool;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("integration")
 class CliIntegrationTest {
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
 
+    @AfterEach
+    public void tearDown() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    /**
+     * Tests the behavior which required options are not provided
+     */
+    @Test
+    public void cliShowErrorWhenRequiredOptionsNotProvideTest() {
+        ByteArrayOutputStream outArray = new ByteArrayOutputStream();
+        ByteArrayOutputStream errArray = new ByteArrayOutputStream();
+        String args[] = {};
+
+        System.setErr(new PrintStream(errArray));
+
+        Main.main(args);
+
+        assertEquals(
+                "Flags -md, -rf, and -o are required, please see the following usage for more details...\n".trim(),
+                errArray.toString().trim()
+        );
+    }
 }
